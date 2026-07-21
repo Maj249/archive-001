6b43a718f4b3c499d56a7e5ac81ec1ba8c6bfd18
Maj249 <jack.magi27@gmail.com>
2026-07-21 23:29:31 +0000

    Update MAGI OS

--- src/data/music.js
+++ src/data/music.js
@@ -4,7 +4,7 @@ const music = {
     "https://open.spotify.com/user/jacky249boy711-au?si=693cb7e223ef4f0a",
   discogsUrl: "https://www.discogs.com/user/Maj711/collection",
   statement:
-    "Music is a big part of who I am and how I work. Check out my shit",
+    "Music is a big part of who I am and how I work.",
 };
 
 export default music;
